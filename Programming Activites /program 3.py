c0062c0898c6d21d2f578b378c09f757d8bc6adb
tso230 <tso411163@gmail.com>
2026-09-09 15:01:59 -0400

    Add program 3.py with basic functions and prints

---
+++ Programming Activites /program 3.py
@@ -0,0 +1,15 @@
+# This program was created in Arduino Lab for MicroPython
+
+# var stores my name
+myName = "TS Ouyang"
+
+# print cmd referring to var
+print(myName)
+
+print('TS Ouyang')
+
+def newFunctionTemplate():
+  print("test")
+  print(True)
+
+newFunctionTemplate()
